a928a5477292b677c9b3ee7effd82a4d31bddcce
Kalpit Patel <pate1148@purdue.edu>
2023-05-23 11:57:47 -0400

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -120,7 +120,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1rK43oWkfnJ8kBXYb-lve-RtD4PY1Hh23/view?usp=sharing' target="_blank"
+              href='https://drive.google.com/file/d/1FZYYQJ89Vl3XPFzKBl9GSZTh9uemNK27/view?usp=sharing' target="_blank"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
